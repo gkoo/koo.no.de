0b9dbbadc94b3ec9e56afe6e0cc5648473031d95
Gordon Koo <gkoo@udp077329uds.ucsf.edu>
2010-12-14 14:48:40 -0800

    added express server on 3000

--- server.js
+++ server.js
@@ -6,3 +6,13 @@ var s = http.createServer(function(req, res) {
   res.end('<h1>hello world</h1>');
 });
 s.listen('80');
+
+// Express app
+express = require('express');
+
+var app = express.createServer();
+app.get('/', function(req, res) {
+  res.send('Hello World Express');
+});
+
+app.listen(3000);
