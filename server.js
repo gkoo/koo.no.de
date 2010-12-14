02c477cd31fbeac28a43a211e9333bfb1f9adf74
Gordon <gordon+git@gordonkoo.com>
2010-12-14 15:55:42 -0800

    made a skeleton express app

--- server.js
+++ server.js
@@ -1,18 +1,44 @@
-// test
-var http = require('http');
 
-var s = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('<h1>hello world</h1>');
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyDecoder());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.staticProvider(__dirname + '/public'));
 });
-//s.listen('80');
 
-// Express app
-express = require('express');
-
-var app = express.createServer();
-app.get('/', function(req, res) {
-  res.send('Hello World Express');
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
-app.listen(80);
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render('index', {
+    locals: {
+      title: 'Express'
+    }
+  });
+});
+
+// Only listen on $ node app.js
+
+if (!module.parent) {
+  app.listen(3000);
+  console.log("Express server listening on port %d", app.address().port)
+}
