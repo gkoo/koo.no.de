1e9cdd8754b2fd757b19f9782d8dee1463fdfba6
Gordon <gordon+git@gordonkoo.com>
2010-12-19 19:46:34 -0800

    added names

--- public/javascripts/io.js
+++ public/javascripts/io.js
@@ -1,4 +1,26 @@
-var socket = new io.Socket(null, {port: 8080, rememberTransport: false}); 
+var username,
+    socket = new io.Socket(null, {port: 8080, rememberTransport: false}),
+    send = function() {
+      var input = document.getElementById('chatinput'),
+          messageObj = { sender: username, msg: input.value };
+      socket.send(messageObj);
+      message({ message: ['', messageObj] });
+      input.value = '';
+    },
+    message = function(obj) {
+      var el = document.createElement('p');
+      if ('announcement' in obj) el.innerHTML = '<em>' + esc(obj.announcement) + '</em>';
+      else if ('message' in obj) { 
+        var messageObj = obj.message[1];
+        el.innerHTML = '<b>' + esc(messageObj.sender) + ':</b> ' + esc(messageObj.msg);
+      }
+      document.getElementById('chat-window').appendChild(el);
+      document.getElementById('chat-window').scrollTop = 1000000;
+    },
+    esc = function(msg) {
+      return String(msg).replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    };
+
 socket.connect();
 socket.on('message', function(obj){
   if ('buffer' in obj){
@@ -7,26 +29,6 @@ socket.on('message', function(obj){
     for (var i in obj.buffer) message(obj.buffer[i]);
   } else message(obj);
 });
-
-var send = function() {
-  var input = document.getElementById('chatinput');
-  var val = input.value;
-  socket.send(val);
-  message({ message: ['you', val]});
-  input.value = '';
-};
-
-var message = function(obj) {
-  var el = document.createElement('p');
-  if ('announcement' in obj) el.innerHTML = '<em>' + esc(obj.announcement) + '</em>';
-  else if ('message' in obj) el.innerHTML = '<b>' + esc(obj.message[0]) + ':</b> ' + esc(obj.message[1]);
-  document.getElementById('chat-window').appendChild(el);
-  document.getElementById('chat-window').scrollTop = 1000000;
-}
-
-var esc = function(msg) {
-  return String(msg).replace(/</g, '&lt;').replace(/>/g, '&gt;');
-};
 
 $(document).ready(function() {
   $('#chatsendbtn').click(function() {
@@ -37,4 +39,5 @@ $(document).ready(function() {
       send();
     }
   });
+  username = window.prompt("What is your name?");
 });
