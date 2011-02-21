2892546ad444ecfe8d885b10dd7367291c8eefe1
Gordon <gordon+git@gordonkoo.com>
2011-02-20 17:09:03 -0800

    added grid section

--- routes.js
+++ routes.js
@@ -1,15 +1,6 @@
 var express = require('express');
     app = express.createServer();
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyDecoder());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.staticProvider(__dirname + '/public'));
-});
-
 // Configuration
 
 app.configure(function(){
@@ -34,7 +25,16 @@ app.configure('production', function(){
 app.get('/', function(req, res){
   res.render('index', {
     locals: {
-      title: 'Guy With A Site'
+      title: 'Some Guy\'s Site'
+    }
+  });
+});
+
+app.get('/grid', function(req, res){
+  res.render('grid', {
+    locals: {
+      dimSize: 30,
+      title: 'Welcome to the Grid'
     }
   });
 });
