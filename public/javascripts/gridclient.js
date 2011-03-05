a7cbfd45657ef21975889c05a08f8c5af8cff806
Gordon Koo <gkoo@gkoo-md.linkedin.biz>
2011-03-04 22:52:23 -0800

    rearranged client js functions due to JSlint

--- public/javascripts/gridclient.js
+++ public/javascripts/gridclient.js
@@ -4,6 +4,125 @@ var socket = null,
     currColorClass = 'black',
     username = '',
     PLAYER_ID_PREFIX = 'player-';
+/*
+ * toggleCell
+ * ==========
+ *
+ * Switches the state of the cell to the opposite state.
+ *
+ * Takes in one argument: cellInfo of type Object.
+ * You can either pass in the HTML element or the coordinates
+ * for the HTML element you want to toggle.
+ */
+
+var getCellByCoord = function(x, y) {
+  // blah! nth-child is 1-indexed. why??
+  var row = $(grid).children('li:nth-child('+(y+1)+')'),
+      col = null;
+  row = row.children('ul');
+  col = row.children('li:nth-child('+(x+1)+')');
+  return col;
+},
+
+getCellFromCellInfo = function(cellInfo) {
+  var cell = null;
+
+  if ('cell' in cellInfo && cellInfo.cell) {
+    cell = $(cellInfo.cell);
+  }
+  else if ('x' in cellInfo && 'y' in cellInfo) {
+    cell = getCellByCoord(cellInfo.x, cellInfo.y);
+  }
+  return cell;
+},
+
+toggleCell = function(cellInfo) {
+  var cell = getCellFromCellInfo(cellInfo);
+  if (!cell) { return; }
+
+  cell.css('display', 'none');
+  cell.removeClass();
+  cellInfo.currColorClass ? cell.addClass(cellInfo.currColorClass) : cell.addClass(currColorClass);
+  cell.css('display', 'inline-block');
+},
+
+getCoordByCell = function(el) {
+  var innerGrid = $(el).parent(),
+      x         = innerGrid.children('li').index(el),
+      y         = $(grid).children('li').index(innerGrid.parent());
+
+  return { x: x, y: y };
+},
+
+/*
+ * isDrawable
+ * ==========
+ * Returns false if the cell matches the user's current color. Otherwise,
+ * returns true.
+ */
+isDrawable = function(el) {
+  return el.get(0).tagName.toLowerCase() === 'li' && !el.hasClass(currColorClass);
+},
+
+doDraw = function(el) {
+  if (mouseIsDown && isDrawable(el)) {
+    var elemCoord = getCoordByCell(el);
+
+    toggleCell({ cell: el });
+    socket.send({
+      type: 'toggle',
+      x: elemCoord.x,
+      y: elemCoord.y,
+      currColorClass: currColorClass,
+      username: username
+    });
+  }
+},
+
+// Adds a name label to the cell that was just drawn.
+addLabel = function(cellInfo) {
+  var cell, newLabel;
+
+  if (typeof cellInfo.username === 'undefined' || !cellInfo.username) {
+    return;
+  }
+  cell = getCellFromCellInfo(cellInfo);
+
+  existingName = $('#' + PLAYER_ID_PREFIX + cellInfo.playerid);
+  if (existingName) {
+    existingName.remove();
+  }
+
+  newLabel = $('<div>').text(cellInfo.username)
+                       .addClass('nameLabel')
+                       .attr('id', PLAYER_ID_PREFIX + cellInfo.playerId);
+  cell.append(newLabel);
+
+  setTimeout(function() {
+    if (newLabel) { newLabel.remove(); }
+  }, 2000);
+},
+
+handleNameChange = function() {
+  var newName = $('#editNameText').val(), // user input! be sure to escape!
+      MAX_NAME_LENGTH = 20;
+  if (newName.length > MAX_NAME_LENGTH) {
+    alert('Sorry, your name can\'t be more than ' + MAX_NAME_LENGTH + ' characters long.');
+  }
+  else {
+    // Change name and hide field.
+    username = newName;
+    nameElem = $('#username');
+    $('#editNameField').hide();
+    if (newName) {
+      nameElem.children('span').text(newName).show();
+    }
+    else {
+      nameElem.children('span').hide();
+    }
+    nameElem.show();
+  }
+};
 
 $(document).ready(function() {
 
@@ -28,7 +147,7 @@ $(document).ready(function() {
         // Do initialization of grid, if there is any to be done.
         for (var i=0; i<obj.grid.length; ++i) { // i: columns
           for (var j=0; j<obj.grid[i].length; ++j) { // j: rows
-            if (obj.grid[i][j] != 0) {
+            if (obj.grid[i][j] !== 0) {
               var cell = getCellByCoord(i, j);
               toggleCell({ cell: cell, currColorClass: obj.grid[i][j] });
             }
@@ -111,122 +230,3 @@ $(document).ready(function() {
 
 });
 
-/*
- * toggleCell
- * ==========
- *
- * Switches the state of the cell to the opposite state.
- *
- * Takes in one argument: cellInfo of type Object.
- * You can either pass in the HTML element or the coordinates
- * for the HTML element you want to toggle.
- */
-
-var toggleCell = function(cellInfo) {
-  var cell = getCellFromCellInfo(cellInfo);
-  if (!cell) { return; }
-
-  cell.css('display', 'none');
-  cell.removeClass();
-  cellInfo.currColorClass ? cell.addClass(cellInfo.currColorClass) : cell.addClass(currColorClass);
-  cell.css('display', 'inline-block');
-};
-
-var getCellByCoord = function(x, y) {
-  // blah! nth-child is 1-indexed. why??
-  var row = $(grid).children('li:nth-child('+(y+1)+')'),
-      col = null;
-  row = row.children('ul');
-  col = row.children('li:nth-child('+(x+1)+')');
-  return col;
-};
-
-var getCoordByCell = function(el) {
-  var innerGrid = $(el).parent(),
-      x         = innerGrid.children('li').index(el),
-      y         = $(grid).children('li').index(innerGrid.parent());
-
-  return { x: x, y: y };
-};
-
-var getCellFromCellInfo = function(cellInfo) {
-  var cell = null;
-
-  if ('cell' in cellInfo && cellInfo.cell) {
-    cell = $(cellInfo.cell);
-  }
-  else if ('x' in cellInfo && 'y' in cellInfo) {
-    cell = getCellByCoord(cellInfo.x, cellInfo.y);
-  }
-  return cell;
-}
-
-var doDraw = function(el) {
-  if (mouseIsDown && isDrawable(el)) {
-    var elemCoord = getCoordByCell(el);
-
-    toggleCell({ cell: el });
-    socket.send({
-      type: 'toggle',
-      x: elemCoord.x,
-      y: elemCoord.y,
-      currColorClass: currColorClass,
-      username: username
-    });
-  }
-};
-
-// Adds a name label to the cell that was just drawn.
-var addLabel = function(cellInfo) {
-  var cell, newLabel;
-
-  if (typeof cellInfo.username === 'undefined' || !cellInfo.username) {
-    return;
-  }
-  cell = getCellFromCellInfo(cellInfo);
-
-  existingName = $('#' + PLAYER_ID_PREFIX + cellInfo.playerid);
-  if (existingName) {
-    existingName.remove();
-  }
-
-  newLabel = $('<div>').text(cellInfo.username)
-                       .addClass('nameLabel')
-                       .attr('id', PLAYER_ID_PREFIX + cellInfo.playerId);
-  cell.append(newLabel);
-
-  setTimeout(function() {
-    if (newLabel) { newLabel.remove(); }
-  }, 2000);
-}
-
-/*
- * isDrawable
- * ==========
- * Returns false if the cell matches the user's current color. Otherwise,
- * returns true.
- */
-var isDrawable = function(el) {
-  return el.get(0).tagName.toLowerCase() === 'li' && !el.hasClass(currColorClass);
-};
-
-var handleNameChange = function() {
-  var newName = $('#editNameText').val(), // user input! be sure to escape!
-      MAX_NAME_LENGTH = 20;
-  if (newName.length > MAX_NAME_LENGTH) {
-    alert('Sorry, your name can\'t be more than ' + MAX_NAME_LENGTH + ' characters long.');
-  }
-  else {
-    // Change name and hide field.
-    username = newName;
-    nameElem = $('#username');
-    $('#editNameField').hide();
-    if (newName) {
-      nameElem.children('span').text(newName).show();
-    }
-    else {
-      nameElem.children('span').hide();
-    }
-    nameElem.show()
-  }
-};
